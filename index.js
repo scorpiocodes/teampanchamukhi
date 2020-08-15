178f740bdee039441dc63e6428afcbdb15a4962c
iamharshdev <nitingoswami473@gmail.com>
2020-08-15 17:00:18 +0000

    still idk

--- index.js
+++ index.js
@@ -3,47 +3,40 @@ var nodemailer = require('nodemailer');
 var cors = require('cors');
 
 var app = express();
-app.use('/static', express.static('./static'));
+app.use(express.static(__dirname + '/'));
 var port = 3000;
 app.use(cors);
 app.use(express.json());
-app.listen(port, () => {
-	console.log('Listenening on:', port);
-});
 
 app.get('/', (req, res) => {
-	res.sendFile('index.html', { root: './' });
+	res.send('Hello');
 });
 
 const transporter = nodemailer.createTransport({
-	service: 'gmail',
+	host: 'smtp.gmail.com',
+	port: 465,
+	secure: true,
 	auth: {
 		user: 'teampanchamukhiwelfaresociety@gmail.com',
 		pass: 'team@987', // naturally, replace both with your real credentials or an application-specific password
 	},
-});
-
-// verify connection configuration
-transporter.verify(function (error, success) {
-	if (error) {
-		console.log(error);
-	} else {
-		console.log('Server is ready to take our messages');
-	}
-});
+})
 
 app.post('/send', (req, res) => {
 	var name = req.body.name;
 	var email = req.body.email;
 	var subject = req.body.subject;
 	var message = req.body.message;
-	var mailOptions = {
-		from: email,
+    console.log(req.body);
+    
+	// Specify what the email will look like
+	const mailOpts = {
+		from: email, // This is ignored by Gmail
 		to: 'teampanchamukhiwelfaresociety@gmail.com',
 		subject: subject,
-		text: `Hello myself ${name}, ${message}`,
+		text: `${name} (${email}) says: ${message}`,
 	};
-	transporter.sendMail(mailOptions, (err, data) => {
+	transporter.sendMail(mailOpts, (err, data) => {
 		if (err) {
 			res.json({
 				status: 'fail',
@@ -55,4 +48,8 @@ app.post('/send', (req, res) => {
 			});
 		}
 	});
+});
+
+app.listen(port, () => {
+	console.log('Listenening on:', port);
 });
